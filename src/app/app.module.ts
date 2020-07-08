ff9188c97ee95fd3d4cbe1dbbe05f83f24d1fe39
Donald Emeka Achugo <achugo.donald@gmail.com>
2020-07-08 04:27:25 +0200

    feat(spinner-component): added first impl of spinner component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,16 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SearchComponent } from './search/search.component';
 import { ContactsComponent } from './contacts/contacts.component';
+import { AddContactComponent } from './add-contact/add-contact.component';
+import { SpinnerComponent } from './spinner/spinner.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     SearchComponent,
     ContactsComponent,
+    AddContactComponent,
+    SpinnerComponent,
   ],
   imports: [
     BrowserModule,
